bc85242ce5e3d25fc9fbb02ee2eafc89130eb177
Ahshan Habib <ahshanhabib1216@gmail.com>
2025-03-14 12:44:56 +0600

    officeupdate

--- Office Work/3rd day/JWT_Auth_todo/JWT_auth_todo/credential/urls.py
+++ Office Work/3rd day/JWT_Auth_todo/JWT_auth_todo/credential/urls.py
@@ -4,6 +4,7 @@ from .views import SignupView, LoginView, TodoView
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
-    path('dashboard/', TodoView.as_view(), name='dashboard')
+    path('dashboard/', TodoView.as_view(), name='dashboard'),
+    path('dashboard/<int:pk>/', TodoView.as_view(), name='dashboard')
     
 ]
--- Office Work/3rd day/JWT_Auth_todo/JWT_auth_todo/credential/views.py
+++ Office Work/3rd day/JWT_Auth_todo/JWT_auth_todo/credential/views.py
@@ -53,4 +53,36 @@ class TodoView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk):
+       
+        todo = Todo.objects.get(pk=pk)
+
+        if request.user.role == 'teacher':
+             serializer = TodoSerializer(todo, data=request.data, partial=True)
+        elif request.user.role == 'student':
+            
+            allowed_fields = {'is_completed'}
+            attempted_fields = set(request.data.keys())
+
+            if not attempted_fields.issubset(allowed_fields):
+                return Response(
+                        {"detail": "You do not have permission to edit fields you only edit is_completed field."},
+                    status=status.HTTP_403_FORBIDDEN
+            )
+            student_data = {'is_completed': request.data.get('is_completed', todo.is_completed)}
+            serializer = TodoSerializer(todo, data=student_data, partial=True)
+            
+        else:
+            return Response(
+                {"detail": "You do not have permission to update a Todo."},
+                status=status.HTTP_403_FORBIDDEN
+            )
+            
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+             
+          
           
